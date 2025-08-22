c198c021e652cb9cd7064fd8b046139d0afbc217
Marco Jara Schulz <mjaraschulz@gmail.com>
2025-08-22 16:19:04 -0400

    feat: hola mundo desde django
    actividad clase 2 backend donde creamos nuestro primer hola mundo

---
+++ bienvenida/bienvenida/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse("Hola mundo desde Django")
+
+def mostrar_bievenida(request):
+    name="Marco Jara"
+    return HttpResponse(f"Bienvenidos a mi primera App Django, {name}!")
